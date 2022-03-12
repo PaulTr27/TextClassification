e6cc0db52a0176e30456b76232341e392a88eb3d
PaulTran2734 <84268848+PaulTran2734@users.noreply.github.com>
2022-03-12 09:31:35 +0700

    Add files via upload

--- TxtClassApp.py
+++ TxtClassApp.py
@@ -1,58 +1,65 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
+
+
+# import streamlit and transformers
+import transformers
+import streamlit as st  
+
+
+
+# In[ ]:
+
+
+
+# In[ ]:
+
+
+## create streamlit framework
+def main():
+    st.title('Text Classification Streamlit App')
+
+    if 'count' not in st.session_state:
+        st.session_state.count = 1
+        st.write('Press Load GUI to continue')
+        st.write(st.session_state.count)
+        from transformers import pipeline
+        with st.spinner('Loading...'):
+            classifier = pipeline("zero-shot-classification", device=0)
+            default_labels = ['Positive','Negative','Neutral']
 
 
 
 
-# ### Streamlit interface
-
-# In[2]:
+    load = st.button('Load GUI')
 
 
+    if load or st.session_state.count == 1:
 
-
-# In[3]:
-
-
-import tensorflow as tf
-
-
-# In[4]:
-
-
-from transformers import pipeline
-classifier = pipeline("zero-shot-classification", device=0)
-
-
-# In[6]:
-
-
-import streamlit as st
-import numpy as np
-
-st.title('Feedback')
-
-
-text = 'The weather is nice today'
-candidate_labels = ["positive", "negative", "neutral"]
-user_input = st.text_input("Text", text)
-doc = classifier(user_input, candidate_labels)
-
-x = np.array(doc['scores'])
-b = doc['labels']
-
-m = np.char.find(b, 'positive')
-m = np.where(m == 0 )
-n = np.char.find(b, 'negative')
-n = np.where(n == 0 )
-k = np.char.find(b, 'neutral')
-k = np.where(k == 0 )
-
-st.write('Positive:', round(x[m][0], 2))
-st.write('Negative:', round(x[n][0], 2)) 
-st.write('Neutral:', round(x[k][0], 2)) 
+        st.header('Zero-shot training model demonstration')
+        st.write('Default labels: Positive, Negative, Neutral')
+        st.subheader('Do you want to use custom labels?')
+        user_labels = [] #labels for classification
+        choice = st.checkbox(label='Pick yes to use custom labels')
+        if choice:
+            label_no = st.slider('Number of labels',3,5,1,1)
+            for i in range(label_no):
+                label = st.text_input("Label number {} here".format(i+1))
+                user_labels.append(label)
+        else: 
+            user_labels = default_labels
+        st.subheader("Input your sentence")
+        user_input = st.text_input("Type your sentence here","This is an amazing day!")
+        pred_class = classifier(user_input,user_labels)['labels'][0]
+        st.write('Your sentence belongs in class: ',pred_class)
+        st.write(st.session_state.count)
+        
+    
+if __name__ == 'main' :
+    main()
+            
 
 
 # In[ ]:
